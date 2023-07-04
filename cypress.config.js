aaec1f539e5a0bd456b68dedf862e7fef6ed3928
Jules <jules.douet@gmail.com>
2023-07-04 09:34:36 +0200

    Made cypress open devtool by default

--- cypress.config.js
+++ cypress.config.js
@@ -57,6 +57,29 @@ module.exports = defineConfig({
         },
       };
       on("file:preprocessor", webpackPreprocessor(options));
+      on("before:browser:launch", (browser = {}, launchOptions) => {
+        // `args` is an array of all the arguments that will
+        // be passed to browsers when it launches
+        console.log(launchOptions.args); // print all current args
+
+        if (browser.family === "chromium" && browser.name !== "electron") {
+          // auto open devtools
+          launchOptions.args.push("--auto-open-devtools-for-tabs");
+        }
+
+        if (browser.family === "firefox") {
+          // auto open devtools
+          launchOptions.args.push("-devtools");
+        }
+
+        if (browser.name === "electron") {
+          // auto open devtools
+          launchOptions.preferences.devTools = true;
+        }
+
+        // whatever you return here becomes the launchOptions
+        return launchOptions;
+      });
       return config;
     },
     baseUrl: "http://localhost:9200/",
