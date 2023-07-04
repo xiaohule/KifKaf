b0097d149cefa6c546fab00aebfa273e75a6f8a2
Jules <jules.douet@gmail.com>
2023-07-04 09:28:37 +0200

    Trying to fix app check distant

--- src/boot/firebaseBoot.js
+++ src/boot/firebaseBoot.js
@@ -27,6 +27,10 @@ self.FIREBASE_APPCHECK_DEBUG_TOKEN =
   process.env.APP_CHECK_DEBUG_TOKEN_FROM_CI ||
   process.env.NODE_ENV === "development" ||
   false;
+console.log(
+  "FIREBASE_APPCHECK_DEBUG_TOKEN",
+  self.FIREBASE_APPCHECK_DEBUG_TOKEN
+);
 // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
 // key is the counterpart to the secret key you set in the Firebase console.
 const appCheck = initializeAppCheck(firebaseApp, {
