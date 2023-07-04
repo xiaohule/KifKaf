0dddf15ce5dc6cca772371e1ca17696243c98574
Jules <jules.douet@gmail.com>
2023-07-04 09:17:06 +0200

    Fixed App Check tokens

--- src/boot/firebaseBoot.js
+++ src/boot/firebaseBoot.js
@@ -20,6 +20,13 @@ const firebaseApp = initializeApp(firebaseConfig);
 export const db = getFirestore(firebaseApp);
 export const auth = getAuth(firebaseApp);
 export const analytics = getAnalytics(firebaseApp);
+
+//APP CHECK
+// Set FIREBASE_APPCHECK_DEBUG_TOKEN to the CI one if CI, true (to use the dev whitelisted ones) if no in CI but in dev and false otherwise
+self.FIREBASE_APPCHECK_DEBUG_TOKEN =
+  process.env.APP_CHECK_DEBUG_TOKEN_FROM_CI ||
+  process.env.NODE_ENV === "development" ||
+  false;
 // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
 // key is the counterpart to the secret key you set in the Firebase console.
 const appCheck = initializeAppCheck(firebaseApp, {
