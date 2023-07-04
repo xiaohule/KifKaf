b616e3d34afec0fe500ce69a95b4fad904ffe6ff
Jules <jules.douet@gmail.com>
2023-07-04 11:53:56 +0200

    showing all uncaught errors

--- test/cypress/support/e2e.js
+++ test/cypress/support/e2e.js
@@ -16,18 +16,18 @@
 import "./commands";
 import "@cypress/code-coverage/support";
 
-Cypress.on("uncaught:exception", (err, runnable) => {
-  // we expect a 3rd party library error with message 'list not defined'
-  // and don't want to fail the test so we return false
-  if (
-    err.message.includes(
-      "Failed to fetch dynamically imported module: http://localhost:9200/src/pages/HomeTab.vue"
-    ) ||
-    err.message.includes("ResizeObserver loop limit exceeded") ||
-    err.message.includes("cancelled")
-  ) {
-    return false;
-  }
-  // we still want to ensure there are no other unexpected
-  // errors, so we let them fail the test
-});
+// Cypress.on("uncaught:exception", (err, runnable) => {
+//   // we expect a 3rd party library error with message 'list not defined'
+//   // and don't want to fail the test so we return false
+//   if (
+//     err.message.includes(
+//       "Failed to fetch dynamically imported module: http://localhost:9200/src/pages/HomeTab.vue"
+//     ) ||
+//     err.message.includes("ResizeObserver loop limit exceeded") ||
+//     err.message.includes("cancelled")
+//   ) {
+//     return false;
+//   }
+//   // we still want to ensure there are no other unexpected
+//   // errors, so we let them fail the test
+// });
