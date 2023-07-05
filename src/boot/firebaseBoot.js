0509d42f400600f3e68b8facdd06cab14fc5f226
Jules <jules.douet@gmail.com>
2023-07-05 19:26:57 +0200

    renaming of the project

--- src/boot/firebaseBoot.js
+++ src/boot/firebaseBoot.js
@@ -27,6 +27,10 @@ self.FIREBASE_APPCHECK_DEBUG_TOKEN =
   process.env.CYPRESS_APP_CHECK_DEBUG_TOKEN_FROM_CI ||
   process.env.NODE_ENV === "development" ||
   false;
+console.log(
+  "FIREBASE_APPCHECK_DEBUG_TOKEN",
+  self.FIREBASE_APPCHECK_DEBUG_TOKEN
+);
 // Pass your reCAPTCHA v3 site key (public key). Make sure this key is the counterpart to the secret key you set in the Firebase console.
 const appCheck = initializeAppCheck(firebaseApp, {
   provider: new ReCaptchaV3Provider("6Lcwc_AmAAAAALodsOgDWM_0W3Ts1yrj_SKoPEfB"),
