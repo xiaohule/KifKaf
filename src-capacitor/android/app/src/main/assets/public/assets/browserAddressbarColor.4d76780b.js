649a6b75b161ab64c4e2bc7229d2ccd05f02fbab
Jules <jules.douet@gmail.com>
2023-09-23 10:19:05 +0200

    removed android

--- src-capacitor/android/app/src/main/assets/public/assets/browserAddressbarColor.4d76780b.js
+++
@@ -1,1 +0,0 @@
-import{A as r}from"./index.904fb14a.js";var s=()=>{r.set("#f7f7f7")};export{s as default};
--- src-capacitor/android/app/src/main/assets/public/assets/use-quasar.c5871871.js
+++
@@ -1,1 +0,0 @@
-import{i as a,aM as r}from"./index.904fb14a.js";function u(){return a(r)}export{u};
