5cb8804a0a5f3a48a94de8b0dd7bb7469f8c9052
Jules <jules.douet@gmail.com>
2023-10-22 12:32:23 +0200

    trying to fix distant tests

--- test/cypress/e2e/home.cy.js
+++ test/cypress/e2e/home.cy.js
@@ -70,7 +70,7 @@ describe("Navigating sign in screens & Signing up > out > in", () => {
       cy.get("#inbox-id").invoke("text");
     }).as("username");
 
-    cy.visit("/");
+    cy.visit("/", { timeout: 60000 });
     //should have sign in options, ToS and Contact us
     cy.contains("Log in", { timeout: 60000 }).should("be.visible").click();
     cy.contains("arrow_back").should("be.visible").click();
