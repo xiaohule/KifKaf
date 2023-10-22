df8f04b03cbf83d004e095ac026e7646b1811808
Jules <jules.douet@gmail.com>
2023-10-22 19:40:00 +0200

    reverted optimizeDeps config

--- quasar.config.js
+++ quasar.config.js
@@ -83,15 +83,15 @@ module.exports = configure(function (ctx) {
       // polyfillModulePreload: true,
       // distDir
 
-      extendViteConf(viteConf) {
-        viteConf.optimizeDeps = {
-          entries: [
-            "tests/cypress/**/*.js",
-            "src/**/*.{ts,js,vue}",
-            "index.html",
-          ],
-        };
-      },
+      // extendViteConf(viteConf) {
+      //   viteConf.optimizeDeps = {
+      //     entries: [
+      //       "tests/cypress/**/*.js",
+      //       "src/**/*.{ts,js,vue}",
+      //       "index.html",
+      //     ],
+      //   };
+      // },
       viteVuePluginOptions: {
         template: {
           compilerOptions: {
